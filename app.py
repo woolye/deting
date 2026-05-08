8e547b4583d9c05b1fce3f96e01ba007babf5310
woolye <amoguscoolcatluckyfan@gmail.com>
2026-05-08 21:02:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
-from flask import Flask
+from http.server import BaseHTTPRequestHandler
 
-app = Flask(__name__)
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
 
-@app.route("/")
-def home():
-    return """
-    <!doctype html>
-    <html>
-    <head>
-      <style>
-        html, body {
-          margin: 0;
-          height: 100%;
-          background: black;
-          overflow: hidden;
-        }
-        iframe {
-          width: 100vw;
-          height: 100vh;
-          border: none;
-        }
-      </style>
-    </head>
-    <body>
-      <iframe
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0"
-        allow="autoplay; fullscreen"
-        allowfullscreen>
-      </iframe>
-    </body>
-    </html>
-    """
+        html = """
+        <!doctype html>
+        <html>
+        <head>
+          <style>
+            html, body {
+              margin: 0;
+              height: 100%;
+              background: black;
+              overflow: hidden;
+            }
+            iframe {
+              width: 100vw;
+              height: 100vh;
+              border: none;
+            }
+          </style>
+        </head>
+        <body>
+          <iframe
+            src="https://www.youtube.com/embed/2RWKJn8S9gg?autoplay=1&controls=0&rel=0&mute=1"
+            allow="autoplay; fullscreen"
+            allowfullscreen>
+          </iframe>
+        </body>
+        </html>
+        """
 
-app = app
+        self.wfile.write(html.encode())
